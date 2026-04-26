f2d4bd0a67da1c909ce95c035a03ae94f24c8a12
nina-penot <nina.penot@laplateforme.io>
2026-04-26 16:10:23 +0200

    Continuation Zonetest
    - Modification pour générer un array grille lorsque l'image charge
    - A FAIRE: Transformer en élément canvas, créer et utiliser des pièces éléments prédéfinis clickables (buissons, arbres etc) au lieu d'utiliser une grille

--- FRONTEND/src/components/ZoneTest/ZoneTest.jsx
+++ FRONTEND/src/components/ZoneTest/ZoneTest.jsx
@@ -1,10 +1,53 @@
-import useGameData from "../../hooks/useGameData"
+import { useState, useEffect } from "react";
+import useGameData from "../../hooks/useGameData";
+import zoneimg from "../../assets/images/zones/zonetest.png";
 
 export default function ZoneTest() {
 
     const { zonedata, getzones } = useGameData();
 
+    // const [zonestate, setZonestate] = useState({});
+    const [zonetilesarray, setZonetilesarray] = useState([]);
+
+
+    let save = true;
+    useEffect(() => {
+        if (save) {
+            saveinState();
+        }
+        save = false;
+    }, [])
+
+    function saveinState() {
+        getzones();
+    }
+
+    function getImageSize(e) {
+        console.log(e.target.width, "*", e.target.height);
+        const imgWidth = e.target.width;
+        const imgHeight = e.target.height;
+
+        const tile = 64;
+
+        const tilesAmt_width = imgWidth / tile;
+        const tilesAmt_height = imgHeight / tile;
+
+        let tilearray = [];
+        for (let h = 0; h < tilesAmt_height; h++) {
+            tilearray.push([]);
+            for (let w = 0; w < tilesAmt_width; w++) {
+                tilearray[h].push(0);
+            }
+        }
+
+        console.log(tilearray);
+    }
+
     //getzones();
-    console.log("zonedata:",);
+    console.log("zonedata:", zonedata);
+
+    return (
+        <img src={zonedata.zonedata && zonedata.zonedata[0].img} onLoad={getImageSize}></img>
+    )
 
 }
--- FRONTEND/src/hooks/useGameData.js
+++ FRONTEND/src/hooks/useGameData.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { gameService } from "../services/api";
 
 export default function useGameData() {
